1470256cbe402c42de2f648dc0ae8d567b35d588
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-06-21 15:25:03 +0600

    default route added

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors'
 import bookRoute from './modules/book/book.controller';
 import borrowRoute from './modules/borrow/borrow.controller';
@@ -8,6 +8,10 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/", bookRoute)
-app.use("/",borrowRoute)
+app.use("/", borrowRoute)
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("This a Library Management Server")
+})
 
 export default app;
