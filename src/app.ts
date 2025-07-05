f907f866b2a2901572e319b3ffea5e529eac7faa
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-07-05 08:26:19 +0600

    cors url modified

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://library-management-ui-zeta.vercel.app"],
   })
 );
 
