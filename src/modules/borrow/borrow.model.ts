b28a73b88f251c99ea1ea62fc1dbe12c593ce4cc
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-07-05 12:43:23 +0600

    borrow model modified

--- src/modules/borrow/borrow.model.ts
+++ src/modules/borrow/borrow.model.ts
@@ -3,7 +3,6 @@ import { IBorrow } from "./borrow.interface";
 import Book from "../book/book.model";
 import { borrowStaticMethods } from "../borrow/borrow.interface";
 
-
 const borrowSchema = new Schema<IBorrow, borrowStaticMethods>(
   {
     book: { type: SchemaTypes.ObjectId, ref: "Book" },
@@ -25,13 +24,13 @@ const borrowSchema = new Schema<IBorrow, borrowStaticMethods>(
   }
 );
 
-// pre hook 
+// pre hook
 borrowSchema.pre("save", async function () {
   const book = await Book.findById(this.book);
-  if (book && book.copies===0) {
-  return  new Error("Book copies  unavailable")
+  if (book && book.copies === 0) {
+    return;
   }
-})
+});
 
 //post hook
 borrowSchema.post("save", async function () {
@@ -41,19 +40,19 @@ borrowSchema.post("save", async function () {
     await book.save();
   }
   await Borrow.updateBookAvailability(this.book);
-})
+});
 
 // static methods
 borrowSchema.static(
   "updateBookAvailability",
   async function (bookId: Types.ObjectId) {
     const book = await Book.findById(bookId);
-      
-      if (book && book.copies <= 0) {
-        book.available = false;
-        await book.save();
-      }     
+
+    if (book && book.copies <= 0) {
+      book.available = false;
+      await book.save();
     }
+  }
 );
 const Borrow = mongoose.model<IBorrow, borrowStaticMethods>(
   "Borrow",
